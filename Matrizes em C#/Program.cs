b67d772716ecaeaff520a918cd9070d386790bb7
igorl <igorl@DESKTOP-273U8TR>
2024-05-20 18:14:58 -0300

    Adicionar arquivos de projeto.

---
+++ Matrizes em C#/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrizes_em_C_ {
+    internal class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int M, N;
+
+            Console.Write("Quantas linhas vai ter a matriz? ");
+            M = int.Parse(Console.ReadLine());
+            Console.Write("Quantas colunas vai ter a matriz? ");
+            N = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[M, N]; ///int[,] Definir matriz em mais dimensões
+
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    Console.Write("Elemento [" + i + "," + j + "]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("MATRIZ DIGITADA:");
+            for(int i = 0;i < M; i++) {
+                for(int j = 0;j < N; j++) {
+                    Console.Write(mat[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
